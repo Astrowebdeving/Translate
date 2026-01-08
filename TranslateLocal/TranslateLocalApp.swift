2230f2c3d95deeed1b9c06ee6c06c3bd3017c795
Alexander Tu <oceania1984ingsoc@yahoo.com>
2026-01-08 17:28:30 -0600

    Initial Commit

---
+++ TranslateLocal/TranslateLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateLocalApp.swift
+//  TranslateLocal
+//
+//  Created by Manghui Tu on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TranslateLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
